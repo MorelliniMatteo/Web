1008689099e1faeed5d181d04e511502d20816ef
Matteo Morellini <matteomorellini92@gmail.com>
2023-12-11 11:06:34 +0100

    things added

--- ProgettoWebAle/src/template/registrazione.php
+++ ProgettoWebAle/src/template/registrazione.php
@@ -1,40 +1,40 @@
-            <section>
-                <h2>Registrazione</h2>
-                <?php if(isset($templateParams["messaggio"])): ?>
-                    <p><?php echo $templateParams["messaggio"]; ?></p>
-                <?php endif; ?>
-                <form method="POST">
-                    <fieldset>
-                    <ul>
-                        <li>
-                            <label for="nomeRegistrazione">Nome:</label><br/>
-                            <input required type="text" id="nomeRegistrazione" name="nomeRegistrazione" />
-                        </li>
-                        <li>
-                            <label for="cognomeRegistrazione">Cognome:</label><br/>
-                            <input required type="text" id="cognomeRegistrazione" name="cognomeRegistrazione" />
-                        </li>
-                        <li>
-                            <label for="emailRegistrazione">Email:</label><br/>
-                            <input required type="text" id="emailRegistrazione" name="emailRegistrazione" />
-                        </li>
-                        <li>
-                            <label for="telefonoRegistrazione">Numero di telefono:</label><br/>
-                            <input required type="text" id="telefonoRegistrazione" name="telefonoRegistrazione" />
-                        </li>
-                        <li>
-                            <label for="passwordRegistrazione">Password:</label><br/>
-                            <input required type="password" id="passwordRegistrazione" name="passwordRegistrazione" />
-                        </li>
-                        <li>
-                            <label for="confermaPassword">Conferma password:</label><br/>
-                            <input required type="password" id="confermaPassword" name="confermaPassword" />
-                        </li>
-                        <li>
-                            <input type="submit" name="invioRegistrazione" value="Invia" />
-                        </li>
-                    </ul>
-                    </fieldset>
-                </form>
-                <p>Se sei già registrato allora <a href="login.php">Accedi!</a></p>
-            </section>
+<section>
+    <h2>Registrazione</h2>
+    <?php if(isset($templateParams["messaggio"])): ?>
+        <p><?php echo $templateParams["messaggio"]; ?></p>
+    <?php endif; ?>
+    <form method="POST">
+        <fieldset>
+        <ul>
+            <li>
+                <label for="nomeRegistrazione">Nome:</label><br/>
+                <input required type="text" id="nomeRegistrazione" name="nomeRegistrazione" />
+            </li>
+            <li>
+                <label for="cognomeRegistrazione">Cognome:</label><br/>
+                <input required type="text" id="cognomeRegistrazione" name="cognomeRegistrazione" />
+            </li>
+            <li>
+                <label for="emailRegistrazione">Email:</label><br/>
+                <input required type="text" id="emailRegistrazione" name="emailRegistrazione" />
+            </li>
+            <li>
+                <label for="telefonoRegistrazione">Numero di telefono:</label><br/>
+                <input required type="text" id="telefonoRegistrazione" name="telefonoRegistrazione" />
+            </li>
+            <li>
+                <label for="passwordRegistrazione">Password:</label><br/>
+                <input required type="password" id="passwordRegistrazione" name="passwordRegistrazione" />
+            </li>
+            <li>
+                <label for="confermaPassword">Conferma password:</label><br/>
+                <input required type="password" id="confermaPassword" name="confermaPassword" />
+            </li>
+            <li>
+                <input type="submit" name="invioRegistrazione" value="Invia" />
+            </li>
+        </ul>
+        </fieldset>
+    </form>
+    <p>Se sei già registrato allora <a href="login.php">Accedi!</a></p>
+</section>
